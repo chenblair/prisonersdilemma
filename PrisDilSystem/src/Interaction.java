9c0d13e5186733304996000953dcc20ed724fc02
unbrace3 <unbrace3@gmail.com>
2016-01-27 19:11:12 -0800

    Implement Repeated Interactions
    for when you want more than just one

--- PrisDilSystem/src/Interaction.java
+++ PrisDilSystem/src/Interaction.java
@@ -3,8 +3,14 @@ public class Interaction
 {
 	boolean prevA1=false,prevA2=false;
 	int prevS1=0,prevS2=0;
+	Player a,b;
 	Rewards reward=new Rewards();
-	public void interact(Player a,Player b)
+	public Interaction (Player a1,Player b1)
+	{
+		a=a1;
+		b=b1;
+	}
+	public void interact()
 	{
 		prevS1=reward.getScore1(a.respond(prevA2,prevS1),b.respond(prevA1,prevS2));
 		prevS2=reward.getScore2();
--- PrisDilSystem/src/RepeatedInteraction.java
+++ PrisDilSystem/src/RepeatedInteraction.java
@@ -1,4 +1,25 @@
-
+import java.util.*;
 public class RepeatedInteraction {
-
+	int ttlReps=100;
+	Map<Integer,Integer> numReps= new HashMap<Integer,Integer>();
+	public void interact(Player a,Player b)
+	{
+		Interaction interaction=new Interaction(a,b);
+		for (int i=0;i<ttlReps;i++)
+		{
+			interaction.interact();
+		}
+	}
+	
+	public boolean controlledInteract(int id,Player a,Player b)
+	{
+		if (numReps.containsKey(id))
+		{
+			if (numReps.get(id).intValue()>100)
+				return false;
+			numReps.put(id,new Integer(numReps.get(id).intValue() + 1));
+		} else
+			numReps.put(id, 1);
+		return true;
+	}
 }
